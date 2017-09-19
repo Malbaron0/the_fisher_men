7f76aa83a8ca20a53329a23c810312b01ba29ce5
Malyar Baron <malbaron0@gmail.com>
2017-09-19 00:14:18 -0400

    Additional components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './css/style.css';
-import StorePicker from './components/StorePicker';
-import App from './App';
 
-ReactDOM.render(<StorePicker/>, document.querySelector('#root'));
+import StorePicker from './components/StorePicker';
+import App from './components/App';
+import './css/style.css';
+
+ReactDOM.render(<App/>, document.querySelector('#main'));
 
 //import registerServiceWorker from './registerServiceWorker';
 
