e000d021d24020f323db7595ef706c2374210c3a
Malyar Baron <malbaron0@gmail.com>
2017-09-18 23:06:51 -0400

    First Component, also addition of styles

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './css/style.css';
+import StorePicker from './components/StorePicker';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<StorePicker/>, document.querySelector('#root'));
+
+//import registerServiceWorker from './registerServiceWorker';
+
+//registerServiceWorker();
