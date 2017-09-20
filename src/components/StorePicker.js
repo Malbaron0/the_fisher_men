476f701280fd1201255888c54e00211eb51bb286
Malyar Baron <malbaron0@gmail.com>
2017-09-19 23:52:25 -0400

    Working with react events

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import {getFunName} from './../helpers';
 
 class StorePicker extends React.Component {
+
+    gotToStore(event) {
+        event.preventDefault();
+        console.log('Url changed');
+    }
+
+
     render() {
         return(
-            <form className="store-selector">
+            <form className="store-selector" onSubmit={this.gotToStore}>
                 <h2>Please Enter A Store</h2>
-                <input type="text" required placeholder = "Store Name"/>
+                <input type="text" required placeholder = "Store Name" defaultValue = {getFunName()}/>
                 <button type="submit">Visit Store →</button>
             </form>
         );
