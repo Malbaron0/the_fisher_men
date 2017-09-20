98e8d014c42d335396b68312e1c88d086a2b5a47
Malyar Baron <malbaron0@gmail.com>
2017-09-19 23:04:39 -0400

    Implement React Router with correct Paths

---
+++ src/components/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+
+    return (
+        <h2>This is not the page you are looking for...</h2>
+    )
+
+}
+
+export default NotFound;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { BrowserRouter, Route, Miss, Switch } from 'react-router-dom';
 import StorePicker from './components/StorePicker';
 import App from './components/App';
 import './css/style.css';
+import NotFound from './components/NotFound';
 
-ReactDOM.render(<App/>, document.querySelector('#main'));
+
+
+const Main = () => {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route exact path='/' component={StorePicker} />
+                <Route path='/store/:storeId' component={App} />
+                <Route component={NotFound}/>
+            </Switch>
+        </BrowserRouter>
+    )
+}
+
+
+
+
+
+ReactDOM.render(<Main />, document.querySelector('#main'));
 
 //import registerServiceWorker from './registerServiceWorker';
 
