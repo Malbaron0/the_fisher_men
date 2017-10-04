b9c70dbeed50c84a7f2ce3bc615906cddcce042b
Malyar Baron <malbaron0@gmail.com>
2017-10-04 15:06:46 -0400

    Adding autthentication - not complete

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import FishForm from './FishForm';
-
+import base from '../fireBase';
+import * as firebase from "firebase";
 
 class Inventory extends React.Component {
     constructor() {
         super();
         this.renderInventory = this.renderInventory.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.renderLogin = this.renderLogin.bind(this);
+        this.authenticate = this.authenticate.bind(this);
+        this.state = {
+            uid: null,
+            owner: null
+
+        }
     }
 
     //need to update state with this method
-    handleChange(e, key){
+    handleChange(e, key) {
         const fish = this.props.fishies[key];
 
         const updateFish = {
@@ -20,6 +29,24 @@ class Inventory extends React.Component {
 
         this.props.updateFish(key, updateFish);
 
+    }
+
+    authenticate(provider){
+        var provider = new firebase.auth.FacebookAuthProvider();
+        
+        
+        firebase.auth().signInWithPopup(provider).then(function(authData) {
+            console.log(authData);
+        }).catch(function(error) {
+            console.log(error);
+        });
+    }
+
+    //call this callback once user has signed in successfully
+    authHandler(err, authData){
+        if (err) throw err;
+
+        console.log(authData);
     }
 
     renderInventory(key) {
@@ -34,16 +61,43 @@ class Inventory extends React.Component {
                 </select>
                 <textarea onChange={e => this.handleChange(e, key)} name="desc" ref={input => this.desc = input} placeholder="Fish Desc"></textarea>
                 <input onChange={e => this.handleChange(e, key)} name="image" ref={input => this.image = input} type="text" placeholder="Fish Image" />
-                <button className = "warning" onClick = {() => this.props.removeFish(key)}>Remove Item</button>
+                <button className="warning" onClick={() => this.props.removeFish(key)}>Remove Item</button>
             </div>
         );
     }
 
+    renderLogin() {
+        return (
+            <nav className="login">
+                <h2>Inventory</h2>
+                <button className="github" onClick={() => this.authenticate("github")}>Log using Github</button>
+                <button className="facebook" onClick={() => this.authenticate("facebook")}>Log using Facebook</button>
+            </nav>
+        )
+    }
+
 
     render() {
+        const logout = <button>Log Out</button>
+
+        if (!this.state.uid) {
+            return <div>{this.renderLogin()}</div>
+        }
+
+        //check if login user is the same as the store owner. 
+        if (this.state.uid !== this.state.owner) {
+            return (
+                <div>
+                    <p>Sorry you are not the owner of this store</p>
+                    {logout}
+                </div>
+            )
+        }
+
         return (
             <div>
                 <h2>Inventory</h2>
+                {logout}
                 {Object.keys(this.props.fishies).map(this.renderInventory)}
                 <FishForm addFish={this.props.addFish} />
                 <button onClick={this.props.loadSamples}>Load Smaple Fishes</button>
