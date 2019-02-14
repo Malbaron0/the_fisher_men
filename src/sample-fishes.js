5cb48f521970d6b339a0bc9d5015807ae7f17685
Malyar Baron <malbaron0@gmail.com>
2019-02-14 18:18:53 -0500

    Rename and refactor image path in sample-fishes.js

--- src/sample-fishes.js
+++ src/sample-fishes.js
@@ -1,7 +1,7 @@
 module.exports = {
   fish1: {
     name: 'Pacific Halibut',
-    image: 'src/imgs/pacific_halibut.jpg',
+    image: 'halibut.jpg',
     desc: 'Everyones favorite white fish. We will cut it to the size you need and ship it.',
     price: 1724,
     status: 'available'
@@ -9,7 +9,7 @@ module.exports = {
 
   fish2: {
     name: 'Lobster',
-    image: 'src/imgs/lobster.jpg',
+    image: 'lobster.jpg',
     desc: 'These tender, mouth-watering beauties are a fantastic hit at any dinner party.',
     price: 3200,
     status: 'available'
@@ -17,7 +17,7 @@ module.exports = {
 
   fish3: {
     name: 'Sea Scallops',
-    image: 'src\imgs\scallops.jpg',
+    image: 'scallops.jpg',
     desc: 'Big, sweet and tender. True dry-pack scallops from the icey waters of Alaska. About 8-10 per pound',
     price: 1684,
     status: 'unavailable'
@@ -25,7 +25,7 @@ module.exports = {
 
   fish4: {
     name: 'Mahi Mahi',
-    image: 'src\imgs\mahi.jpg',
+    image: 'mahi.jpg',
     desc: 'Lean flesh with a mild, sweet flavor profile, moderately firm texture and large, moist flakes. ',
     price: 1129,
     status: 'available'
@@ -33,7 +33,7 @@ module.exports = {
 
   fish5: {
     name: 'King Crab',
-    image: 'src\imgs\king crab.png',
+    image: 'king crab.png',
     desc: 'Crack these open and enjoy them plain or with one of our cocktail sauces',
     price: 4234,
     status: 'available'
@@ -41,7 +41,7 @@ module.exports = {
 
   fish6: {
     name: 'Atlantic Salmon',
-    image: 'src\imgs\salmon.jpg',
+    image: 'salmon.jpg',
     desc: 'This flaky, oily salmon is truly the king of the sea. Bake it, grill it, broil it...as good as it gets!',
     price: 1453,
     status: 'available'
@@ -49,7 +49,7 @@ module.exports = {
 
   fish7: {
     name: 'Oysters',
-    image: 'src\imgs\oyster.jpg',
+    image: 'oyster.jpg',
     desc: 'A soft plump oyster with a sweet salty flavor and a clean finish.',
     price: 2543,
     status: 'available'
@@ -57,7 +57,7 @@ module.exports = {
 
   fish8: {
     name: 'Mussels',
-    image: 'src\imgs\mussels.jpg',
+    image: 'mussels.jpg',
     desc: 'The best mussels from the Pacific Northwest with a full-flavored and complex taste.',
     price: 425,
     status: 'available'
@@ -65,7 +65,7 @@ module.exports = {
 
   fish9: {
     name: 'Jumbo Prawns',
-    image: 'src\imgs\prawns.jpg',
+    image: 'prawns.jpg',
     desc: 'With 21-25 two bite prawns in each pound, these sweet morsels are perfect for shish-kabobs.',
     price: 2250,
     status: 'available'
