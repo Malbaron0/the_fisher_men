fcf0a0efc16873f9ff2f26bd05bbe14c668b7490
Malyar Baron <malbaron0@gmail.com>
2019-02-14 18:18:27 -0500

    implement css styles for fish component

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -1,21 +1,24 @@
 import React from 'react';
-import {formatPrice} from '../helpers';
-
+import { formatPrice } from '../helpers';
 class Fish extends React.Component {
     render() {
         const { details, keyIndex, addToOrder } = this.props;
         const isAvaliable = details.status === 'available';
         const buttonText = isAvaliable ? 'Add To Cart' : 'Sold out! :(';
-        
+        const image = require(`../css/images/${details.image}`);
+        console.log(image);
         return (
             <div className="menu-fish">
-                <img src={details.image} alt={details.name} />
-                <h3 className="fish-name">
-                    {details.name}
-                    <span className="price">{formatPrice(details.price)}</span>
-                </h3>
-                <p>{details.desc}</p>
-
+                <div>
+                    <img src={image} alt={details.name} />
+                </div>
+                <div className="fish-name">                    
+                        {details.name}
+                        <span className="price"><span>$</span>{formatPrice(details.price)}</span>
+                </div>
+                <div className = "fish-desc">
+                    <p>{details.desc}</p>
+                </div>
                 <button onClick={() => addToOrder(keyIndex)} disabled={!isAvaliable}>{buttonText}</button>
             </div>
         )
