6d0d7defa169283afaea2313ab5ed8f77f4ab1dd
Malyar Baron <malbaron0@gmail.com>
2019-03-27 16:13:53 -0400

    Updates

---
+++ precache-manifest.fa888454014929d8c5908e4b7a4dc022.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "af647b282b9cb85bb42ea194cad44368",
+    "url": "/the_fisher_men/static/media/mahi.af647b28.jpg"
+  },
+  {
+    "revision": "9aa3b602b0642bdad416",
+    "url": "/the_fisher_men/static/css/main.03ef711a.chunk.css"
+  },
+  {
+    "revision": "8c7059e4832658f25a84",
+    "url": "/the_fisher_men/static/js/runtime~main.b3466a04.js"
+  },
+  {
+    "revision": "1d313ae30e375ccd3aec",
+    "url": "/the_fisher_men/static/js/2.26d2c186.chunk.js"
+  },
+  {
+    "revision": "d2f8799a13e6db2022d9a7de22776705",
+    "url": "/the_fisher_men/static/media/anchor.d2f8799a.svg"
+  },
+  {
+    "revision": "321c0f2ee19a1aa88d2ca1876f627b63",
+    "url": "/the_fisher_men/static/media/halibut.321c0f2e.jpg"
+  },
+  {
+    "revision": "073673e88229a03db16a12f1a5a8ae27",
+    "url": "/the_fisher_men/static/media/king crab.073673e8.png"
+  },
+  {
+    "revision": "3587eee93eb9ddebcc5caeeb9b2de53e",
+    "url": "/the_fisher_men/static/media/lobster.3587eee9.jpg"
+  },
+  {
+    "revision": "9aa3b602b0642bdad416",
+    "url": "/the_fisher_men/static/js/main.bf1f5493.chunk.js"
+  },
+  {
+    "revision": "71ef43c548fb343530e9097b48739a5d",
+    "url": "/the_fisher_men/static/media/mussels.71ef43c5.jpg"
+  },
+  {
+    "revision": "0f99543ba9f8c476a1761c78127aec11",
+    "url": "/the_fisher_men/static/media/oyster.0f99543b.jpg"
+  },
+  {
+    "revision": "59db8220fbc1f080aae23383c07d29de",
+    "url": "/the_fisher_men/static/media/pacific_halibut.59db8220.jpg"
+  },
+  {
+    "revision": "18538535ee763b99614c7c9d025e6b56",
+    "url": "/the_fisher_men/static/media/prawns.18538535.jpg"
+  },
+  {
+    "revision": "69bbee043154caed62f9fea6e2ae4ba9",
+    "url": "/the_fisher_men/static/media/salmon.69bbee04.jpg"
+  },
+  {
+    "revision": "da74d9733ed18d8383baa5bf2ec27a15",
+    "url": "/the_fisher_men/static/media/scallops.da74d973.jpg"
+  },
+  {
+    "revision": "ede487c642722e220df914b5c772cd4a",
+    "url": "/the_fisher_men/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/the_fisher_men/precache-manifest.fa888454014929d8c5908e4b7a4dc022.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/the_fisher_men/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
